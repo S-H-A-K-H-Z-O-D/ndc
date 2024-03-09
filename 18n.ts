038ec09cb543d1c5dd868c7b1bc8bef1e7efc5d6
S-H-A-K-H-Z-O-D <m.shakhzod2002@gmail.com>
2024-03-10 03:39:45 +0500

    first commit

---
+++ 18n.ts
@@ -0,0 +1,26 @@
+import i18n from 'i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import {initReactI18next} from 'react-i18next'
+import Backend from 'i18next-http-backend'
+
+import EN from './locales/en/en.json'
+import RU from './locales/ru/ru.json'
+import UZ from './locales/uz/uz.json'
+
+const resources = {
+    en: EN,
+    ru: RU,
+    uz: UZ,
+}
+
+i18n.use(Backend)
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        resources,
+        fallbackLng: 'en',
+        saveMissing: true,
+        react: {useSuspense: true},
+    })
+
+export default i18n
